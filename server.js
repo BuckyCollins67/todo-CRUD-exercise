68fce1e60ea2a8c867e08d97f2f34f576b598723
Bucky Collins <bucky.collins.67@gmail.com>
2023-10-23 02:08:08 -0700

    Fully Refactored and working

--- server.js
+++ server.js
@@ -23,70 +23,38 @@ app.use(express.urlencoded({ extended: true }));
 
 //Set Routes
 app.use('/', homeRoutes)
-// app.use('/edit', editRoutes)
+app.use('/edit', editRoutes)
 
-// GET METHOD
-app.get("/", async (req, res) => {
-    try {
-        TodoTask.find({}, (err, tasks) => {
-            res.render("index.ejs", { todoTasks: tasks });
-        });
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-    }
-});
+// // GET METHOD
+// app.get("/", async (req, res) => {
+//     try {
+//         TodoTask.find({}, (err, tasks) => {
+//             res.render("index.ejs", { todoTasks: tasks });
+//         });
+//     } catch (err) {
+//         if (err) return res.status(500).send(err);
+//     }
+// });
 
 //POST METHOD
-app.post('/', async (req, res) => {
-    const todoTask = new TodoTask(
-        {
-            title: req.body.title,
-            content: req.body.content
-        });
-    try {
-        await todoTask.save();
-        console.log(todoTask)
-        res.redirect("/");
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-        res.redirect("/");
-    }
-});
-
-//UPDATE METHOD
-// app
-//     .route("/edit/:id")
-//     .get((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.find({}, (err, tasks) => {
-//             res.render("edit.ejs", { todoTasks: tasks, idTask: id });
+// app.post('/', async (req, res) => {
+//     const todoTask = new TodoTask(
+//         {
+//             title: req.body.title,
+//             content: req.body.content
 //         });
-//     })
-//     .post((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.findByIdAndUpdate(
-//             id,
-//             {
-//                 title: req.body.title,
-//                 content: req.body.content
-//             },
+//     try {
+//         await todoTask.save();
+//         console.log(todoTask)
+//         res.redirect("/");
+//     } catch (err) {
+//         if (err) return res.status(500).send(err);
+//         res.redirect("/");
+//     }
+// });
 
-//             err => {
-//                 if (err) return res.status(500).send(err);
-//                 res.redirect("/");
-//             });
-//     });
 
-// //DELETE
-// app
-//     .route("/remove/:id")
-//     .get((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.findByIdAndRemove(id, err => {
-//             if (err) return res.send(500, err);
-//             res.redirect("/");
-//         });
-//     });
+
 
 //Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
